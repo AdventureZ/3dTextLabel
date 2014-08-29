74061ad7fb8d4d853995ac52a71d88810e46bee7
Treze <darthpoezd@live.ru>
2014-08-29 21:47:34 +1100

    Added Resize text and Font

--- labels.lua
+++ labels.lua
@@ -3,15 +3,17 @@ local readLabel = 0
 addEvent("addLabels", true)
 addEventHandler("addLabels", root, function(label) readLabel=label end)
 
-function create3DTextLabel(text, color, x, y, z, dis, dimens, toelement)
+function create3DTextLabel(text, color, x, y, z, dis, dimens, font, size, toelement)
     if not toelement or not isElement(toelement) then toelement = root end
     if not x or not tonumber(x) then x = 0 end
     if not y or not tonumber(y) then y = 0 end
     if not z or not tonumber(z) then z = 5 end
+    if not font then font = "default-bold" end
+    if not size or not tonumber(size) or tonumber(size) <= 0 then size = 1 end
 
     readLabel = readLabel+1    
     
-    setTimer(function() triggerClientEvent(toelement, "doCreateLabel", toelement, text, color, x, y, z, dis, dimens) end, 100, 1)
+    setTimer(function() triggerClientEvent(toelement, "doCreateLabel", toelement, text, color, x, y, z, dis, dimens, font, size) end, 100, 1)
     
     return readLabel
 end
@@ -22,10 +24,10 @@ function delete3DTextLabel(id, toelement)
     triggerClientEvent(toelement, "doRemoveLabel", toelement, id)
 end
 
-function update3DTextLabel(id, text, color, x, y, z, dist, dimens, toelement)
+function update3DTextLabel(id, text, color, x, y, z, dist, dimens, font, size, toelement)
     if not toelement or not isElement(toelement) then toelement = root end
     
-    triggerClientEvent(toelement, "doUpdateLabel", toelement, id, text, color, x, y, z, dist, dimens)
+    triggerClientEvent(toelement, "doUpdateLabel", toelement, id, text, color, x, y, z, dist, dimens, font, size)
 end 
 
 function attach3DTextLabelToElement(id, element, x, y, z, dimens, toelement)
@@ -41,6 +43,6 @@ function detach3DTextLabel(id, toelement)
 end
 addEventHandler("onResourceStart", root,
     function()
-        local labelz = create3DTextLabel("3D Text Label", 0xFFFF0000, 0, 0, 0, 3000, -1, root)
-        local labels = create3DTextLabel("3D Text Label", 0xFFFF0000, 0, 0, 20, 3000, -1, root)
+        local labelz = create3DTextLabel("3D Text Label", 0xFFFF0000, 0, 0, 0, 3000, -1)
+        local labels = create3DTextLabel("3D Text Label", 0xFFFF0000, 0, 0, 20, 3000, -1)
     end)
